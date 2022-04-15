2ad566b27b865c5ca978558664ad018c81c2d5da
최정필 <wjdvlf1407@gmail.com>
2022-04-15 18:42:59 +0900

    1.043 Ex04 오류해결

--- src/com/javaex/practice/Ex04.java
+++ src/com/javaex/practice/Ex04.java
@@ -6,6 +6,29 @@ public class Ex04 {
 
 	public static void main(String[] args) {
 		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("나이를 입력해주세요");
+		System.out.print("나이: ");
+		int age = sc.nextInt();
+		
+		if (age<19) {							//--> 19세이상 65세 미만에 1번그룹을 넣어주어야 하여 반대로
+			System.out.print("2번그룹");			//    맨 위에서 19세 미만은 2번그룹으로 보내주었다.	  
+			
+		} else if (65<=age) {					//그러면 19세이상의 나이가 남는데 여기서 65세이상을 제거해 아래의 else에 19세 이상 65세 미만의 값을 넘겨주었다.
+			System.out.print("2번그룹");
+		
+		} else {
+			System.out.print("1번그룹");
+			
+		}
+		
+		System.out.println(" 입니다.");
+		
+		sc.close();
+		
+		/*
 		Scanner sc = new Scanner(System.in);
 		System.out.println("나이를 입력해주세요");
 		System.out.print("나이: ");
@@ -25,7 +48,7 @@ public class Ex04 {
 		System.out.println(" 입니다.");
 		
 		sc.close();
-
+		*/
 	}
 
 }
