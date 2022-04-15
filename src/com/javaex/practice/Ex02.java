d71eb494671e95401c84b3c99b1f3e6f34f7e32c
최정필 <wjdvlf1407@gmail.com>
2022-04-15 18:04:18 +0900

    1.02 Ex02 문제풀이 시작

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Ex02 {
+	
+	public static void main(String[] args) {
+		/*
+		int x = 0;
+		
+		//x가 0과 같으면
+		if ( x=0 ) {
+			System.out.println("x=0이다");
+		}*/
+		
+	}
+
+}
