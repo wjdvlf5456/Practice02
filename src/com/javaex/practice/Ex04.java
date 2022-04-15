0072c6b0774224c96b588401e9867ad8af333abd
최정필 <wjdvlf1407@gmail.com>
2022-04-15 18:36:39 +0900

    1.042 Ex04 오류 발견

--- src/com/javaex/practice/Ex04.java
+++ src/com/javaex/practice/Ex04.java
@@ -11,7 +11,7 @@ public class Ex04 {
 		System.out.print("나이: ");
 		int age = sc.nextInt();
 		
-		if (65>age) {
+		if (65>age) {							//--> 여기서 65미만의 정수는 모두 여기서 인식되어 아래의 age>=19를 제대로 인식하지 못한다.
 			System.out.print("1번그룹");
 			
 		} else if (age>=19) {
