8e3e6722e0928a81cb31f27533e340b1c4a9eb2a
최정필 <wjdvlf1407@gmail.com>
2022-04-15 17:57:38 +0900

    1.011 문제풀

--- src/com/javaex/practice/Ex01.java
+++ src/com/javaex/practice/Ex01.java
@@ -4,13 +4,17 @@ public class Ex01 {
 	
 	public static void main(String[] args) {
 		
+		/* 다음 코드는 오류가 있습니다. 오류의 내용을 찾고 올바르게 수정하여 코딩해 보세요.
 		int age = 15;
 		
 		//age가 0살 초과이고 18살 미만 이면 
-		//if(0 < age < 18) {
+		if(0 < age < 18) {							--> 부등호를 동시에 2개 쓰려면 연산자를 사용하거나 if문의 변수를 추가하여 사용하여야 한다.
 			System.out.println("청소년 입니다.");
 			
-		//}
+		}*/
+		
+		
+		
 		
 	}
 
