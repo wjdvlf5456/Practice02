1d7958afc9ebdfa92a07a6509288ff9cf1c8adaf
최정필 <wjdvlf1407@gmail.com>
2022-04-16 17:40:51 +0900

    1.111 Ex11 문제 기초틀

---
+++ src/com/javaex/practice/Ex11.java
@@ -0,0 +1,16 @@
+package com.javaex.practice;
+
+public class Ex11 {
+
+	public static void main(String[] args) {
+		
+		/*
+		문제 11번: 알파벳을 입력받아 자음 모음을 구분하세요.
+		
+		switch~case문을 사용합니다.
+		영문 소문자 이외의 입력값은 고려하지 않습니다.
+		 */
+
+	}
+
+}
