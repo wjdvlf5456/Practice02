bab45704e3fd18555d81a6b0bacaf429afe00bdc
최정필 <wjdvlf1407@gmail.com>
2022-04-16 16:06:17 +0900

    1.081 Ex08 예상안 1

---
+++ src/com/javaex/practice/Ex08.java
@@ -0,0 +1,27 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex08 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		//문제 8번 : 숫자(정) 3개를 입력받아 가장 작은 수를 출력하세요.
+		
+		/*
+	 	먼저 숫자 3개를 입력할 수 있는 칸을 3개 만들어준 후 제일 작은 수만 출력되게 하고 싶으면
+	 	3개의 변수를 연달아 놓고 1>2>3, 2>3>1, 3>1>2 이런 식으로 놓고 이것이 전부 참일 때 마지막 변수만 출력되게 해준다.
+	 	대신 참거짓은  앞에 부등호는 참인지 거짓인지 상관없고 
+		 */
+		
+		
+		
+		
+		
+		sc.close();
+		
+	}
+
+}
