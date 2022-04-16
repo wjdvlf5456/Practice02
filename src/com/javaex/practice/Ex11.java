b80966266294a85acff8f77d097b3425a235c324
최정필 <wjdvlf1407@gmail.com>
2022-04-16 17:56:42 +0900

    1.112 Ex11 출력전

--- src/com/javaex/practice/Ex11.java
+++ src/com/javaex/practice/Ex11.java
@@ -1,5 +1,7 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex11 {
 
 	public static void main(String[] args) {
@@ -10,6 +12,37 @@ public class Ex11 {
 		switch~case문을 사용합니다.
 		영문 소문자 이외의 입력값은 고려하지 않습니다.
 		 */
+		
+		/*
+		문제풀이 
+		
+		1. 변수는 하나만 입력하고 글자를 하나만 입력하니 char을 사용해준다.   ---->  char을 사용하려 했으나 nextChar이 없어 String sc.nextLine(); 을 사용하였다.
+		2. switch 안에서 case 를 여러개 만든 후 자음과 모음은 break로 나누어준다.
+		
+		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("알파벳 1글자(소문자)를 입력하세요.");
+		
+		System.out.print("알파벳:");
+		String alphabet = sc.nextLine();
+		
+		switch (alphabet) {						//--> 문자 입력때는 "" 사이에 입력
+			case "a": 
+			case "e": 
+			case "i": 
+			case "o": 
+			case "u": 
+				System.out.println("모음입니다.");
+				break;
+	
+			default:
+				System.out.println("자음입니다.");
+				break;
+		}
+		
+		sc.close();
 
 	}
 
