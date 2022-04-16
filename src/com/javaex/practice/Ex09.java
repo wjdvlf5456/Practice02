65c66ab8032a426733efe3af4e3a747575d7b546
최정필 <wjdvlf1407@gmail.com>
2022-04-16 16:47:26 +0900

    1.092 Ex09 문제 예상안

--- src/com/javaex/practice/Ex09.java
+++ src/com/javaex/practice/Ex09.java
@@ -1,11 +1,30 @@
 package com.javaex.practice;
 
+import java.util.Scanner;
+
 public class Ex09 {
 
 	public static void main(String[] args) {
 		
 		//문제 9번 : 두 개의 정수를 입력받은 후에 작은 수가 큰 수의 약수인지 확인하는 프로그램을 작성하세요.
 		// (0은 입력하지 않습니다.)
+		
+		Scanner sc = new Scanner(System.in);
+		
+		/*
+		1. 두 개의 변수를 입력하는 칸을 사용하고 먼저 큰 수 인지 작은 수 인지 분류한다.
+		2. 그 아래 분류로 나온 수를 작은수로 큰 수를 % 한다.
+		3. %된 값이 0인지 아닌지 true false를 결정한다. 
+		 
+		마지막에 결과를 출력할 때는 '작은 변수' 는 '큰 변수'의 약수가 아니다. 또는 입니다. 라고 출력한다.
+		*/
+		
+		
+		
+		
+		
+		
+		sc.close();
 
 	}
 
