d8e233bf19d284dace8a92a9f28686f0f0f00eb4
최정필 <wjdvlf1407@gmail.com>
2022-04-16 17:16:18 +0900

    1.101 Ex10 기초틀

---
+++ src/com/javaex/practice/Ex10.java
@@ -0,0 +1,24 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex10 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		문제 10번 : 사번(양의 정수)을 입력하면 팀을 확인 할 수 있는 프로그램을 작성하세요.
+		
+		
+		*/
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		sc.close();
+		
+		
+	}
+
+}
