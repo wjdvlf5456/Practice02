ba834cfd27428a54363395c3655cdd789247a92a
최정필 <wjdvlf1407@gmail.com>
2022-04-16 16:40:31 +0900

    1.091 Ex09 문제

---
+++ src/com/javaex/practice/Ex09.java
@@ -0,0 +1,12 @@
+package com.javaex.practice;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		
+		//문제 9번 : 두 개의 정수를 입력받은 후에 작은 수가 큰 수의 약수인지 확인하는 프로그램을 작성하세요.
+		// (0은 입력하지 않습니다.)
+
+	}
+
+}
