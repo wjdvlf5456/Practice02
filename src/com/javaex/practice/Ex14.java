032b87809faf9cc3a40946dd51d361c1d16e27a3
최정필 <wjdvlf1407@gmail.com>
2022-04-17 18:18:47 +0900

    1.141 Ex14 문제틀

---
+++ src/com/javaex/practice/Ex14.java
@@ -0,0 +1,35 @@
+package com.javaex.practice;
+
+import	java.util.Scanner;
+
+public class Ex14 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		문제14번: 다음과 같이 수익을 입력받아 소득세를 계산하는 프로그램을 작성하세요.
+			
+			1. 0원이상 1000만원 이하인 경우 tax: 0.09*수익
+			2. 1000만원 초과 4000만원 이하인 경우 tax: 1000*0.09 + 0.18(수익-1000)
+			3. 4000만원 초과 8000만원 미만인 경우 tax: 1000*0.09 + 3000*0.18 + 0.27*(수익-4000)
+			4. 8000만원 이상인 경우 tax: 1000*0.09 + 3000*0.18 + 4000*0.27 + 0.36*(수익-8000)
+		*/
+		
+		
+		
+		
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		
+		
+		sc.close();
+		
+		
+	}
+
+}
