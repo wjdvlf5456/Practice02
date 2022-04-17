ca7d1fd68bde9d69080c1f9d899b46b7954b9965
최정필 <wjdvlf1407@gmail.com>
2022-04-17 17:19:51 +0900

    1.126 첫 풀이 오류 수정

--- src/com/javaex/practice/Ex12.java
+++ src/com/javaex/practice/Ex12.java
@@ -25,58 +25,65 @@ public class Ex12 {
 		 */
 		
 		// 문제 풀어보기
-		// ===================== 첫번째 풀이 시작 =====================
+		// ===================== 첫번째 풀이 시작 =====================                                    
+		                                                                               
+		// ===================== 입력란 =====================
+		/*
+		
 		Scanner sc = new Scanner(System.in);
 		
-		
-		// ===================== 입력란 =====================
 		System.out.println("출력되는 내용을 입력하세요.");
-		System.out.print("기호: ");
-		String sign = sc.nextLine();
 		
-		System.out.print("숫자1: ");
-		int num1 = sc.nextInt();
+		System.out.print("기호: ");                                                    
+		String sign = sc.nextLine();                                                   
+		                                                                               
+		System.out.print("숫자1: ");                                                           
+		int num1 = sc.nextInt();                                                           
+		                                                                                    
+		System.out.print("숫자2: ");                                                           
+		int num2 = sc.nextInt();                                                           
+		                                                                                    
+		// ===================== 조건문 시작 =====================                                
+		switch (sign) {                                                                     
+			case "+":                                                                          
+				System.out.print("결과는: "+sign);                                                                      
+				System.out.print(num1+num2);                                                  
+				break;                                                              
+	                                                                                
+			case "-":                                                               
+				System.out.print("결과는: "+sign);                                                   
+				System.out.print(num1-num2);                                                  
+				break;                                                                            
+				                                                                                  
+			case "*":                                                                          
+				System.out.print("결과는: "+sign);                                                   
+				System.out.print(num1*num2);                                                  
+				break;                                                                            
+				                                                                                  
+			case "/":                                                                          
+				                                                                                  
+				if (num2==0) {                                                                    
+					System.out.print("계산할 수 없습니다.");			// --> 분모가 숫자 '0모가 숫자 '0' 일시 출력                        
+				}                                                                                 
+				                                                                                  
+				System.out.print("결과는: "+sign);                                                   
+				System.out.print(num1/num2);                                                  
+				break;                                                                            
+				                                                                                  
+			default:                                                                           
+				System.out.println("계산할 수 없는 기호입니다.");                                                
+				break;                                                                            
+		}                                                                                    
+		                                                                                     
+		sc.close();    
+
+		*/
 		
-		System.out.print("숫자2: ");
-		int num2 = sc.nextInt();
+		//  ===================== 조건문 끝 =====================
 		
-		// ===================== 조건문 시작 =====================
-		switch (sign) {
-			case "+":
-				System.out.print(num1+num2);
-				System.out.print("결과는: "+sign);
-				break;
-	
-			case "-":
-				System.out.print("결과는: "+sign);
-				System.out.print(num1-num2);
-				break;
-				
-			case "*":
-				System.out.print("결과는: "+sign);
-				System.out.print(num1*num2);
-				break;
-				
-			case "/":
-				
-				if (num2==0) {
-					System.out.print("계산할 수 없습니다.");			// --> 분모가 숫자 '0' 일시 출력
-				}
-				
-				System.out.print("결과는: "+sign);
-				System.out.print(num1/num2);
-				break;
-				
-			default:
-				System.out.println("계산할 수 없는 기호입니다.");
-				break;
-		}
+		/*
+			===================== 첫번째 풀이 오류 검토 =====================
 		
-		sc.close();
-		
-		// ===================== 조건문 끝 =====================
-		
-		/* 
 		첫 시도에서는 결과가 기호, 숫자1, 숫자2 가 그대로 출력되어 '결과는: *294' 라고 잘못 출력되었다.
 		심지어 분모가 0일때에는 계산할 수 없습니다.결과는: /Exception in thread "main" java.lang.ArithmeticException: / by 
 		zero
@@ -88,6 +95,56 @@ public class Ex12 {
 		'결과는:' 뒤에 sign 변수를 넣을 필요가 없었고 switch조건문 안에 있는 int 변수 앞에 double을 붙여주어야 했다.
 		
 		 */
+		
+		//	===================== 첫번째 풀이 수정 후 출력  =====================
+        
+		// ===================== 입력란 =====================
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("출력되는 내용을 입력하세요.");
+		
+		System.out.print("기호: ");                                                    
+		String sign = sc.nextLine();                                                   
+		                                                                               
+		System.out.print("숫자1: ");                                                           
+		int num1 = sc.nextInt();                                                           
+		                                                                                    
+		System.out.print("숫자2: ");                                                           
+		int num2 = sc.nextInt();                                                           
+		                                                                                    
+		// ===================== 조건문 시작 =====================                                
+		switch (sign) {                                                                     
+			case "+":                                                                          
+				System.out.print("결과는: ");                                                                      
+				System.out.print((double)num1+(double)num2);                                                  
+				break;                                                              
+	                                                                                
+			case "-":                                                               
+				System.out.print("결과는: ");                                                   
+				System.out.print((double)num1-(double)num2);                                                  
+				break;                                                                            
+				                                                                                  
+			case "*":                                                                          
+				System.out.print("결과는: ");                                                   
+				System.out.print((double)num1*(double)num2);                                                  
+				break;                                                                            
+				                                                                                  
+			case "/":                                                                          
+				                                                                                  
+				if (num2==0) {
+					System.out.print("계산할 수 없습니다.");			// --> 분모가 숫자 '0모가 숫자 '0' 일시 출력                     
+				}                                                                                 
+				break;                                                                            
+				                                                                                  
+			default:                                                                           
+				System.out.println("계산할 수 없는 기호입니다.");                                                
+				break;                                                                            
+		}                                                                                    
+		                                                                                     
+		sc.close();   
+		
+		//	===================== 첫번째 풀이 수정 후 출력 끝 =====================
 		
 		// ===================== 첫번째 풀이 끝  ======================
 		
